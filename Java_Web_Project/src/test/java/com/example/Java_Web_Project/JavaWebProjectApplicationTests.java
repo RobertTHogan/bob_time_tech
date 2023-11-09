667bd052d54c185796e4ec1a7f40e500292ec3ff
Bob Hogan <147057995+RobertTHogan@users.noreply.github.com>
2023-11-09 07:12:49 -0500

    Add files via upload

---
+++ Java_Web_Project/src/test/java/com/example/Java_Web_Project/JavaWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_Web_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
